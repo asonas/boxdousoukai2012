d498eb03e385db1eded8f9f24a73dea648a7884f
asonas <hzw1258@gmail.com>
2012-01-03 17:54:56 +0900

    最初のコミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boxdousoukai::Application.config.secret_token = '212c829a6b918473e4f1bc264ef663f7adc5adf64cf200dadc0b97694a18bbd02cbf6a2bb0a1caf816fb616e05a82b9248410c02c7e5f8fae62482ec5268b918'
